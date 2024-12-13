bffa03b88129e2bd18e9d24f7fbf3c7004151a86
Kjeldino07 <kjeldvankampen@icloud.com>
2024-12-13 11:19:56 +0100

    More progress on speedometer

--- src/Player.java
+++ src/Player.java
@@ -7,7 +7,6 @@ public class Player {
 
     String imageFile = "resource/auto me.png";
     String fuelTankIcon = "resource/fuel tank 60-60.png";
-    String speedometer = "resource/Speedometer.png";
     String nummers = "resource/nummers.png";
 
     int width = 65;
@@ -93,14 +92,8 @@ public class Player {
 
     }
 
-    public void drawSpeedometer() {
-        SaxionApp.drawImage(speedometer, xS, yS);
-        int speed = Math.max(0, Math.min(track.speed, 99));
-        int ten = speed / 10;
-        int ones = speed % 10;
 
 
-    }
 
     public void draw() {
         InputCheck();
--- src/Track.java
+++ src/Track.java
@@ -3,13 +3,20 @@ import nl.saxion.app.SaxionApp;
 public class Track {
     String imageFile = "resource/baan stil.png";
     String fuelBar = "resource/tankbar.png";
+    String speedometer = "resource/Speedometer.png";
 
     int speed = 10;
     int x, y = 0;
     int width = BasicGame.screenWidth;
     int height = BasicGame.screenHeight;
 
-    public void drawFuelBar(){SaxionApp.drawImage(fuelBar, 0, -50, 670, 780);}
+    public void drawFuelBar() {
+        SaxionApp.drawImage(fuelBar, 0, -50, 670, 780);
+    }
+
+    public void drawSpeedometer() {
+        SaxionApp.drawImage(speedometer, 50, 250);
+    }
 
     public void drawFirst() {
         SaxionApp.drawImage(imageFile, x, y, width, height);
@@ -19,14 +26,37 @@ public class Track {
         SaxionApp.drawImage(imageFile, x, y - height, width, height);
     }
 
+    public void drawSpeedNumber(int x, int y, int digitWidth, int digitHeight) {
+
+        String[] numberImages = new String[10];
+        for (int i = 0; i <= 9; i++) {
+            numberImages[i] = "resource/numbers/" + i + ".png";
+        }
+
+        // convert to string
+        String speedStr = Integer.toString(speed);
+        int currentX = x;
+
+        for (int i = 0; i < speedStr.length(); i++) {
+            int digit = Character.getNumericValue(speedStr.charAt(i));
+
+            SaxionApp.drawImage(numberImages[digit], currentX, y, digitWidth, digitHeight);
+            currentX += digitWidth;
+        }
+
+    }
+
     public void draw() {
         drawFirst();
         drawSecond();
         drawFuelBar();
+        drawSpeedNumber(50, 50, 50, 50);
+        //drawSpeedometer();
         y += speed;
         if (y > BasicGame.screenHeight) {
             y -= height;
         }
     }
+
 
 }
